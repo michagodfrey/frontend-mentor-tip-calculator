d924db87832d7e32d75d5b5589255daf8acac45c
Michael Godfrey <m.godfrey59@gmail.com>
2022-01-22 21:40:49 +1100

    Commit before submission to Frontend Mentor

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 ## Welcome! 👋
 
-Thanks for checking out this front-end coding challenge.
+Thanks for checking out my solution to the tip calculator. This challenge is by Frondend Mentor. I made this over a few days during #100DaysOfCode in January 2022.
 
 [Frontend Mentor](https://www.frontendmentor.io) challenges help you improve your coding skills by building realistic projects.
 
@@ -24,14 +24,16 @@ Users should be able to:
 ### Links
 
 - Solution URL: [Add solution URL here](https://your-solution-url.com)
-- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+- Live Site URL: https://michagodfrey.github.io/frontend-mentor-tip-calculator/
 
 ### Built with
 
 - Semantic HTML5 markup
 - CSS custom properties
-- Flexbox
-- CSS Grid
+- JavaScript
 - Mobile-first workflow
+- BEM naming applied to CSS classes
 
 ### What I learned
+
+I got a lot out of this challenge, the more subtle styles took some fine tuning with the CSS and I got a lot of JavaScript practice. I realise the JavaScript I've used here is not efficient and I have some work and revision to do on JavaScript. 
--- app.js
+++ app.js
@@ -1,30 +1,22 @@
-
-
-// tip calculation
-document.getElementById("form").onchange = function() {
-
-  // check if tip is entered with button or custom tip input
+// tip calculation function, triggers when an input field changes
+document.getElementById("form").oninput = function() {
+  // tip variables, either selectTip (buttons) or customTip (input) will be used for tip value
   let tip;
+  let selectTip = document.getElementById("tip-select").value;
   let customTip = Number(document.getElementById("tip-custom").value);
 
-
-  let setTip = (tip) => {
-     return tip;
-  }
-
-  let selectTip = tip;
-
-  // remove .btn-active class if custom tip used and set tip to customTip
-  // else if selected tip is used set tip to that
-  if (customTip > 0) {
-    let removeActiveState = document.querySelectorAll(".btn--active");
+  // if custom tip input used, use that, otherwise use select tip if already selected
+  if (customTip >= 0) {
+    let removeActiveState = document.querySelectorAll(".tip-btn--active");
     for (let i = 0; i < removeActiveState.length; i++) {
-      removeActiveState[i].classList.remove("btn--active");
+      removeActiveState[i].classList.remove("tip-btn--active");
     }
     tip = customTip;
     selectTip = 0;
   } else if (selectTip > 0) {
     tip = selectTip;
+  } else {
+    tip = customTip;
   }
 
   // bill and people input variables
@@ -32,7 +24,7 @@ document.getElementById("form").onchange = function() {
   let people = Number(document.getElementById("people").value);
 
   // do tip calculation
-  let tipPerPerson = (bill * (tip/100)) / people;
+  let tipPerPerson = (bill * (tip / 100)) / people;
   let totalPerPerson = tipPerPerson + (bill / people);
 
   // display 'can't be zero' if bill input is zero
@@ -53,13 +45,13 @@ document.getElementById("form").onchange = function() {
     document.getElementById("people-zero").style.display = "none";
   }
 
-  // darken reset button if nothing to reset
+  // darken reset button if nothing to reset, otherise set it to active class
   if (tip === 0 && bill === 0 && people === 0) {
-    document.getElementById("reset").classList.remove("btn--reset");
-    document.getElementById("reset").classList.add("btn--deactive");
+    document.getElementById("reset").classList.remove("btn-reset");
+    document.getElementById("reset").classList.add("btn-deactive");
   } else {
-    document.getElementById("reset").classList.remove("btn--deactive");
-    document.getElementById("reset").classList.add("btn--reset");
+    document.getElementById("reset").classList.remove("btn-deactive");
+    document.getElementById("reset").classList.add("btn-reset");
   }
 
   // initialize output values to 0.00 prevent the output from displaying null and infinity
@@ -74,28 +66,59 @@ document.getElementById("form").onchange = function() {
 
 };
 
-// set tip with buttons and stylize on click
+// set tip with buttons and calculate tip amounts
 document.getElementById("buttons").addEventListener("click", e => {
   if (e.target.tagName === "BUTTON") {
-    let removeActiveState = document.querySelectorAll(".btn--active");
+    // edit button styles to display tip amount and reset cutom tip input value
+    let removeActiveState = document.querySelectorAll(".tip-btn--active");
     for (let i = 0; i < removeActiveState.length; i++) {
-      removeActiveState[i].classList.remove("btn--active");
+      removeActiveState[i].classList.remove("tip-btn--active");
     }
-    e.target.className = "btn btn--active";
+    // activate reset button
+    document.getElementById("reset").classList.remove("btn-deactive");
+    document.getElementById("reset").classList.add("btn-reset");
+    // set active style on selected tip button
+    e.target.className = "tip-btn tip-btn--active";
+    // clear custom tip input
     document.getElementById("tip-custom").value = "";
+
+    // assign tip value for calculations
+    let tip = e.target.value;
+
+    // assign value to hidden input field, this value is used in the oninput function above
+    document.getElementById("tip-select").value = tip;
+
+    // bill and people input variables
+    let bill = Number(document.getElementById("bill").value);
+    let people = Number(document.getElementById("people").value);
+
+    // do tip calculation
+    let tipPerPerson = (bill * (tip / 100)) / people;
+    let totalPerPerson = tipPerPerson + (bill / people);
+
+    // initialize output values to 0.00 prevent the output from displaying null and infinity
+    if (isNaN(tipPerPerson) || !isFinite(tipPerPerson)) {
+      tipPerPerson = 0.00;
+      totalPerPerson = 0.00;
+    }
+
+    // display values
+    document.getElementById("display-tip").value = `$${tipPerPerson.toFixed(2)}`;
+    document.getElementById("display-total").value = `$${totalPerPerson.toFixed(2)}`;
   }
 });
 
 // reset tip button and input styles on reset
 document.getElementById("reset").addEventListener("click", () => {
-  let removeActiveState = document.querySelectorAll(".btn--active");
+  let removeActiveState = document.querySelectorAll(".tip-btn--active");
   for (let i = 0; i < removeActiveState.length; i++) {
-    removeActiveState[i].classList.remove("btn--active");
+    removeActiveState[i].classList.remove("tip-btn--active");
   }
   document.getElementById("bill").classList.remove("error");
   document.getElementById("bill-zero").style.display = "none";
   document.getElementById("people").classList.remove("error");
   document.getElementById("people-zero").style.display = "none";
-  document.getElementById("reset").classList.remove("btn--reset");
-  document.getElementById("reset").classList.add("btn--deactive");
+  document.getElementById("reset").classList.remove("btn-reset");
+  document.getElementById("reset").classList.add("btn-deactive");
+  document.getElementById("tip-select").value = 0;
 });
